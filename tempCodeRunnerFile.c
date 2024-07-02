b2d04de066741866319e6ccba8ae164936c23791
Pavan Kumar Vishwakarma <138128486+pavann122@users.noreply.github.com>
2024-07-02 13:29:13 +0530

    Update tempCodeRunnerFile.c

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 void main(){
+    //initiated two variables n and i=0 beacuse we want to start the loop from 0 to n//
     int n,i=0;
-    printf("nter number");
+    printf("Enter number");
     scanf("%d",&n);
     while(i<n){
         printf("%d\n",i*i);
